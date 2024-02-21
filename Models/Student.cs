05e2369053460570135a38ccae9b65781e577b95
JayvonK <jaykhuth@gmail.com>
2024-02-20 20:36:41 -0800

    model

---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+namespace studentInfoHub.Models;
+    public class Student
+    {
+        public int Id {get; set;}
+        public string Name {get; set;} = "Ken";
+
+        public string Grade {get; set;} = "N/A";
+    }
