b8acd4ca4b2243111f5faa705f4a48922db3bb56
JayvonK <jaykhuth@gmail.com>
2024-02-24 23:26:20 -0800

    fromat

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,11 @@
 
 namespace studentInfoHub.Models;
-    public class Student
-    {
-        public int Id {get; set;}
-        public string Name {get; set;} = "No Student";
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "No Student";
 
-        public string Grade {get; set;} = "N/A";
+    public string Grade { get; set; } = "N/A";
 
     internal List<Student> ToList()
     {
